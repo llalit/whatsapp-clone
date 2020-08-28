663ee55354e6659b8836dbbb57778cbb58add8cd
llalit <erlalit.lahoria@gmail.com>
2020-08-28 14:26:21 +0530

    Updates

---
+++ precache-manifest.fbb8618a6542a7ca92b1272879b04b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c74058334e4371d2b696b055a6f26c5",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "9ec355cccf12538a45ee",
+    "url": "/whatsapp-clone/static/css/main.8533123d.chunk.css"
+  },
+  {
+    "revision": "b92dd53c2a36b601dcb0",
+    "url": "/whatsapp-clone/static/js/2.fb0ea1f1.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.fb0ea1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec355cccf12538a45ee",
+    "url": "/whatsapp-clone/static/js/main.ddb1f525.chunk.js"
+  },
+  {
+    "revision": "a7958b95dcdb3bb2147f",
+    "url": "/whatsapp-clone/static/js/runtime-main.05ad3e77.js"
+  }
+]);
